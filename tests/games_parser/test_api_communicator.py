bf81ad4a1b3b24a80edd2c45f2a6d6608e107211
miskibin <michal.skibinski@nokia.com>
2023-09-11 00:08:57 +0200

    Frontend refactor and various bug fixes

--- tests/games_parser/test_api_communicator.py
+++ tests/games_parser/test_api_communicator.py
@@ -11,14 +11,11 @@ class TestApiCommunicator:
     @pytest.fixture(autouse=True)
     def setup(self):
         self.logger = get_logger(lvl=10, logger_name="test_api_communicator")
+        ApiCommunicator.__abstractmethods__ = set()  # doctest: +ELLIPSIS
         self.communicator = ApiCommunicator(self.logger)
         games_file = Path(__file__).parent / "validation_files" / "games.txt"
         with open(games_file) as file:
             self.games = file.read().split("\n\n\n")[:-1]
-
-    def test_get_games(self):
-        with pytest.raises(NotImplementedError):
-            self.communicator.get_games("some name", 10, "blitz")
 
     def test_games_generator(self):
         generator = self.communicator.games_generator("some name", self.games)
