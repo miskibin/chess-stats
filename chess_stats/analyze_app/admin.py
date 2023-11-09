1145383a1759ac51ab034d7e62961098fa7c04e3
michalskibinski109 <michalskibinski109@gmail.com>
2023-11-09 14:20:11 +0100

    small fixes

--- chess_stats/analyze_app/admin.py
+++ chess_stats/analyze_app/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
+from .models import Report, SingleGamePlayer, ChessGame
 
-from . import models
 
-# Register your models here.
-admin.site.register(models.ChessGame)
-admin.site.register(models.Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "chess_com_username",
+        "lichess_username",
+        "time_class",
+        "games_num",
+        "analyzed_games",
+        "engine_depth",
+        "fail_reason",
+        "professional",
+    )
+    search_fields = ("chess_com_username", "lichess_username")
+
+
+class SingleGamePlayerAdmin(admin.ModelAdmin):
+    list_display = ("elo",)
+
+
+class ChessGameAdmin(admin.ModelAdmin):
+    list_display = (
+        "date",
+        "host",
+        "opening",
+        "opening_short",
+        "player_color",
+        "result",
+        "end_reason",
+        "time_class",
+        "time_control",
+        "url",
+        "username",
+    )
+    search_fields = ("host", "opening", "username")
+
+
+admin.site.register(Report, ReportAdmin)
+admin.site.register(SingleGamePlayer, SingleGamePlayerAdmin)
+admin.site.register(ChessGame, ChessGameAdmin)
